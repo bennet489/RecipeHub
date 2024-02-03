8ace634f195eb9630ace3798e74d0edfa6b057c6
TechieGold <techiegold101@gmail.com>
2024-02-03 06:55:59 +0100

    add wtf-form

---
+++ app/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, BooleanField, SubmitField
+from wtforms.validators import Length, DataRequired, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    confirm_password = PasswordField('confirm_password', validators=[DataRequired(),
+                                                                     EqualTo(password)])
+    submit = SubmitField('signUp')
+    
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('Remeber me')
+    submit = SubmitField('Login')
