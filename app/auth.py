5674ee9ea4fcf5fa04258f905ccaa23e258c335d
Gold Israel <techiegold101@gmail.com>
2024-02-11 06:58:12 +0100

    add more conditions to login and registration routes

--- app/auth.py
+++ app/auth.py
@@ -1,26 +1,51 @@
-from flask import render_template, request, flash, redirect, url_for
-from .models import User
-from app.forms import RegistrationForm, LoginForm
+from app import app, db
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
-from . import db, app, bcrypt
-from flask_login import login_user, current_user
+import sqlalchemy as sa
+from flask import request
+from urllib.parse import urlsplit
 
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Account created', 'success')
-        return redirect(url_for('login'))
-    return render_template('signup.html', title='signUp', form=form)
-
+ # Login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
-    if form.validate_on_submit:
-        pass
-    return render_template('login.html', title='login')    
+    if form. validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.email == form.email.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid email or password')
+            return redirect(url_for('login'))
+        else:
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or urlsplit(next_page).netloc != '':
+                next_page = url_for('home')
+                return redirect(next_page)
+            flash('Login successfull')
+            return redirect(url_for('home'))
+    return render_template('login.html', title='login', form=form)
+
+# Register route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(full_name=form.full_name.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created, pls login')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='register', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
