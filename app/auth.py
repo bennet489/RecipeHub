2424a3246fbc57fc90f84fcd2896d678dfd685f2
Gold Israel <techiegold101@gmail.com>
2024-02-17 06:27:06 +0100

    fixed error

--- app/auth.py
+++ app/auth.py
@@ -36,7 +36,7 @@ def register():
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(full_name=form.full_name.data, email=form.email.data)
+        user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
